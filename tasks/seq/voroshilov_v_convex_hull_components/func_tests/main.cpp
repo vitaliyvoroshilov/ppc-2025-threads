d7258e6ce18adf48d024dc91ca1ac98f2f5fb22a
vitaliivoroshilov <vitallizer@yandex.ru>
2025-02-26 19:31:13 +0300

    image 2

--- tasks/seq/voroshilov_v_convex_hull_components/func_tests/main.cpp
+++ tasks/seq/voroshilov_v_convex_hull_components/func_tests/main.cpp
@@ -295,21 +295,21 @@ TEST(voroshilov_v_convex_hull_components_seq, imageTest0Incorrect) {
 
   ASSERT_FALSE(ImageRunTest(src_path, exp_path));
 }
-*/
+
 TEST(voroshilov_v_convex_hull_components_seq, imageTest1) {
   std::string src_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/1_image.png");
   std::string exp_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/1_expected.png");
 
   ASSERT_TRUE(ImageRunTest(src_path, exp_path));
 }
-/*
+*/
 TEST(voroshilov_v_convex_hull_components_seq, imageTest2) {
   std::string src_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/2_image.png");
   std::string exp_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/2_expected.png");
 
   ASSERT_TRUE(ImageRunTest(src_path, exp_path));
 }
-
+/*
 TEST(voroshilov_v_convex_hull_components_seq, imageTest3) {
   std::string src_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/3_image.png");
   std::string exp_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/3_expected.png");
