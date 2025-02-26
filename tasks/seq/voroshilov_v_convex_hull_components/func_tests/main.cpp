8036417842b10b0d7db17bf4533bbabe709bf09b
vitaliivoroshilov <vitallizer@yandex.ru>
2025-02-26 20:24:15 +0300

    image5

--- tasks/seq/voroshilov_v_convex_hull_components/func_tests/main.cpp
+++ tasks/seq/voroshilov_v_convex_hull_components/func_tests/main.cpp
@@ -327,10 +327,10 @@ TEST(voroshilov_v_convex_hull_components_seq, imageTest4) {
 TEST(voroshilov_v_convex_hull_components_seq, imageTest5) {
   std::string src_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/5_image.png");
   std::string exp_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/5_expected.png");
-  
+
   ASSERT_TRUE(ImageRunTest(src_path, exp_path));
-  }
-/*  
+}
+/*
 TEST(voroshilov_v_convex_hull_components_seq, imageTest6) {
   std::string src_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/6_image.png");
   std::string exp_path = ppc::util::GetAbsolutePath("seq/voroshilov_v_convex_hull_components/data/6_expected.png");
