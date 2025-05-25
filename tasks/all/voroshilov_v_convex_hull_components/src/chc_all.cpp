170c8804e09ca44b1c5734fb4087ccfcb39dd5b3
vitaliivoroshilov <vitallizer@yandex.ru>
2025-05-25 13:21:23 +0300

    perf failed

--- tasks/all/voroshilov_v_convex_hull_components/src/chc_all.cpp
+++ tasks/all/voroshilov_v_convex_hull_components/src/chc_all.cpp
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 #include <boost/mpi/communicator.hpp>
+#include <chrono>
+#include <iostream>
 #include <utility>
 #include <vector>
 
@@ -11,11 +13,18 @@ using namespace voroshilov_v_convex_hull_components_all;
 
 bool voroshilov_v_convex_hull_components_all::ChcTaskALL::ValidationImpl() {
   if (world_.rank() == 0) {
+    auto start = std::chrono::high_resolution_clock::now();
+
     int *ptr = reinterpret_cast<int *>(task_data->inputs[0]);
     int height = *ptr;
     ptr = reinterpret_cast<int *>(task_data->inputs[1]);
     int width = *ptr;
     int pixels_size = static_cast<int>(task_data->inputs_count[0]);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "\n Proc" << world.rank() << ", Validation: " << duration << " ms \n";
+
     return height > 0 && width > 0 && (height * width) == pixels_size;
   }
   return true;
@@ -23,6 +32,8 @@ bool voroshilov_v_convex_hull_components_all::ChcTaskALL::ValidationImpl() {
 
 bool voroshilov_v_convex_hull_components_all::ChcTaskALL::PreProcessingImpl() {
   if (world_.rank() == 0) {
+    auto start = std::chrono::high_resolution_clock::now();
+
     int *ptr = reinterpret_cast<int *>(task_data->inputs[0]);
     int height = *ptr;
 
@@ -35,23 +46,36 @@ bool voroshilov_v_convex_hull_components_all::ChcTaskALL::PreProcessingImpl() {
 
     Image image(height, width, pixels);
     imageIn_ = image;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "\n Proc" << world.rank() << ", PreProcessing: " << duration << " ms \n";
   }
   return true;
 }
 
 bool voroshilov_v_convex_hull_components_all::ChcTaskALL::RunImpl() {
   std::vector<Component> components;
+
+  auto start = std::chrono::high_resolution_clock::now();
+
   if (world_.rank() == 0) {
     components = FindComponentsOMP(imageIn_);
   }
 
   hullsOut_ = QuickHullAllMPIOMP(components, imageIn_.width);
 
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+  std::cout << "\n Proc" << world.rank() << ", Run: " << duration << " ms \n";
+
   return true;
 }
 
 bool voroshilov_v_convex_hull_components_all::ChcTaskALL::PostProcessingImpl() {
   if (world_.rank() == 0) {
+    auto start = std::chrono::high_resolution_clock::now();
+
     std::pair<std::vector<int>, std::vector<int>> packed_out = PackHulls(hullsOut_, imageIn_);
     std::vector<int> hulls_indexes = packed_out.first;
     std::vector<int> pixels_indexes = packed_out.second;
@@ -59,6 +83,10 @@ bool voroshilov_v_convex_hull_components_all::ChcTaskALL::PostProcessingImpl() {
     std::ranges::copy(hulls_indexes, reinterpret_cast<int *>(task_data->outputs[0]));
     std::ranges::copy(pixels_indexes, reinterpret_cast<int *>(task_data->outputs[1]));
     task_data->outputs_count[0] = hullsOut_.size();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "\n Proc" << world.rank() << ", PostProcessing: " << duration << " ms \n";
   }
   return true;
 }
