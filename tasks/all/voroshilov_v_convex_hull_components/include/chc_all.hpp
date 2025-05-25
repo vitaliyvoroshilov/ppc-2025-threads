8d142db294c6bc5b8463495da28b342fa5f31958
Vitaliy Voroshilov <vitallizer@yandex.ru>
2025-05-25 04:41:00 +0300

    vrodee

--- tasks/all/voroshilov_v_convex_hull_components/include/chc_all.hpp
+++ tasks/all/voroshilov_v_convex_hull_components/include/chc_all.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <boost/mpi/communicator.hpp>
 #include <utility>
 #include <vector>
-#include <boost/mpi/communicator.hpp>
 
 #include "chc.hpp"
 #include "core/task/include/task.hpp"
