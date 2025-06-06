6f78e946a2659a9864d377d47220180e52b4498d
vitaliivoroshilov <vitallizer@yandex.ru>
2025-06-06 21:30:57 +0300

    data sizes in seq and omp

--- tasks/omp/voroshilov_v_convex_hull_components/perf_tests/main.cpp
+++ tasks/omp/voroshilov_v_convex_hull_components/perf_tests/main.cpp
@@ -136,7 +136,7 @@ bool IsHullSubset(Hull& hull_first, Hull& hull_second) {
 TEST(voroshilov_v_convex_hull_components_omp, chc_pipeline_run) {
   int height = 10'000;
   int width = 10'000;
-  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 1000, 100, 500);
+  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 10'000, 25, 50);
 
   int* p_height = &height;
   int* p_width = &width;
@@ -198,7 +198,7 @@ TEST(voroshilov_v_convex_hull_components_omp, chc_pipeline_run) {
 TEST(voroshilov_v_convex_hull_components_omp, chc_task_run) {
   int height = 10'000;
   int width = 10'000;
-  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 1000, 100, 500);
+  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 10'000, 25, 50);
 
   int* p_height = &height;
   int* p_width = &width;
--- tasks/seq/voroshilov_v_convex_hull_components/perf_tests/main.cpp
+++ tasks/seq/voroshilov_v_convex_hull_components/perf_tests/main.cpp
@@ -137,7 +137,7 @@ bool IsHullSubset(Hull& hull_first, Hull& hull_second) {
 TEST(voroshilov_v_convex_hull_components_seq, chc_pipeline_run) {
   int height = 10'000;
   int width = 10'000;
-  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 1000, 100, 500);
+  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 10'000, 25, 50);
 
   int* p_height = &height;
   int* p_width = &width;
@@ -199,7 +199,7 @@ TEST(voroshilov_v_convex_hull_components_seq, chc_pipeline_run) {
 TEST(voroshilov_v_convex_hull_components_seq, chc_task_run) {
   int height = 10'000;
   int width = 10'000;
-  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 1000, 100, 500);
+  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 10'000, 25, 50);
 
   int* p_height = &height;
   int* p_width = &width;
