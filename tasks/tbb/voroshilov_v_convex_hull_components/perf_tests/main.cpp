64da5087e2db4e3d173ddee642ce07db6b1d51ae
vitaliyvoroshilov <vitallizer@yandex.ru>
2025-06-08 03:17:23 +0300

    10k 3000 100 100 34%

--- tasks/tbb/voroshilov_v_convex_hull_components/perf_tests/main.cpp
+++ tasks/tbb/voroshilov_v_convex_hull_components/perf_tests/main.cpp
@@ -136,7 +136,7 @@ bool IsHullSubset(Hull& hull_first, Hull& hull_second) {
 TEST(voroshilov_v_convex_hull_components_tbb, chc_pipeline_run) {
   int height = 10'000;
   int width = 10'000;
-  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 10'000, 25, 50);
+  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 3'000, 100, 100);
 
   int* p_height = &height;
   int* p_width = &width;
@@ -198,7 +198,7 @@ TEST(voroshilov_v_convex_hull_components_tbb, chc_pipeline_run) {
 TEST(voroshilov_v_convex_hull_components_tbb, chc_task_run) {
   int height = 10'000;
   int width = 10'000;
-  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 10'000, 25, 50);
+  std::vector<int> pixels = GenerateRectanglesComponents(width, height, 3'000, 100, 100);
 
   int* p_height = &height;
   int* p_width = &width;
